c2f7e37692da014ab679fd792cc6beae50ad1338
alltheseteeth <alltheseteeth@protonmail.com>
2020-07-06 14:06:48 -0700

    added vars for new smtp server, removed postfix

--- docker/chapo-dev/lemmydev-up.sh
+++ docker/chapo-dev/lemmydev-up.sh
@@ -4,5 +4,6 @@ set -e
 
 export LEMMYDEV_POSTGRES_PW="$(cat secrets/postgres_pw)"
 export LEMMYDEV_JWT_SECRET="$(cat secrets/jwt_secret)"
+export LEMMYDEV_SMTP_PASSWORD="$(cat secrets/smtp_password)"
 
 ./docker-compose.sh up -d
--- docker/chapo-prod/lemmyprod-up.sh
+++ docker/chapo-prod/lemmyprod-up.sh
@@ -4,5 +4,6 @@ set -e
 
 export LEMMYPROD_POSTGRES_PW="$(cat secrets/postgres_pw)"
 export LEMMYPROD_JWT_SECRET="$(cat secrets/jwt_secret)"
+export LEMMYPROD_SMTP_PASSWORD="$(cat secrets/smtp_password)"
 
 ./docker-compose.sh up -d
