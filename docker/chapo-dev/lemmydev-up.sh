a6f4e8e8cb7baa1b4b5f7e94bc98bd582b03d0ab
alltheseteeth <alltheseteeth@protonmail.com>
2020-07-08 15:52:48 -0700

    changed CI to add string, changed stack for secret add

--- docker/chapo-dev/lemmydev-up.sh
+++ docker/chapo-dev/lemmydev-up.sh
@@ -5,5 +5,6 @@ set -e
 export LEMMYDEV_POSTGRES_PW="$(cat secrets/postgres_pw)"
 export LEMMYDEV_JWT_SECRET="$(cat secrets/jwt_secret)"
 export LEMMYDEV_SMTP_PASSWORD="$(cat secrets/smtp_password)"
+export LEMMYDEV_HCAPTCHA_SECRET_KEY="$(cat secrets/hcaptcha_secret_key)"
 
 ./docker-compose.sh up -d
