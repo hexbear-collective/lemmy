f0dab5bac480f2b888e52115c3c2f3948c31f2cd
PorkrollPosadist <porkrollposadist@protonmail.com>
2020-07-15 02:43:47 -0400

    Add access control logic to CreateCommentReport and CreatePostReport

--- server/src/api/report.rs
+++ server/src/api/report.rs
@@ -4,9 +4,11 @@ use crate::{
   db::{
     comment::*,
     comment_view::*,
+    community_view::*,
     post::*,
     post_view::*,
     user::*,
+    Crud,
   },
   websocket::{
     server::{JoinCommunityRoom, SendComment},
@@ -52,6 +54,34 @@ impl Perform for Oper<CreateCommentReport> {
     pool: &DbPool,
     websocket_info: Option<WebsocketInfo>,
   ) -> Result<CommentReportResponse, LemmyError> {
+    let data: &CreateCommentReport = &self.data;
+    
+    // Verify auth token
+    let claims = match Claims::decode(&data.auth) {
+      Ok(claims) => claims.claims,
+      Err(_e) => return Err(APIError::err("not_logged_in").into()),
+    };
+
+    // Check for site ban
+    let user_id = claims.id;
+    let user = blocking(pool, move |conn| User_::read(&conn, user_id)).await??;
+    if user.banned {
+      return Err(APIError::err("site_ban").into());
+    }
+
+    // Fetch comment information from the database
+    let comment_id = data.comment;
+    let comment = blocking
+      (pool, move |conn| CommentView::read(&conn, comment_id, None)).await??;
+
+    // Check for community ban
+    let is_banned =
+      move |conn: &'_ _| CommunityUserBanView::get(conn, user_id,
+						   comment.community_id).is_ok();
+    if blocking(pool, is_banned).await? {
+      return Err(APIError::err("community_ban").into());
+    }
+    
     return Err(APIError::err("comment_report_not_implemented").into());
   }
 }
@@ -65,6 +95,34 @@ impl Perform for Oper<CreatePostReport> {
     pool: &DbPool,
     websocket_info: Option<WebsocketInfo>,
   ) -> Result<PostReportResponse, LemmyError> {
+    let data: &CreatePostReport = &self.data;
+    
+    // Verify auth token
+    let claims = match Claims::decode(&data.auth) {
+      Ok(claims) => claims.claims,
+      Err(_e) => return Err(APIError::err("not_logged_in").into()),
+    };
+
+    // Check for site ban
+    let user_id = claims.id;
+    let user = blocking(pool, move |conn| User_::read(&conn, user_id)).await??;
+    if user.banned {
+      return Err(APIError::err("site_ban").into());
+    }
+
+    // Fetch post information from the database
+    let post_id = data.post;
+    let post = blocking
+      (pool, move |conn| PostView::read(&conn, post_id, None)).await??;
+
+    // Check for community ban
+    let is_banned =
+      move |conn: &'_ _| CommunityUserBanView::get(conn, user_id,
+						   post.community_id).is_ok();
+    if blocking(pool, is_banned).await? {
+      return Err(APIError::err("community_ban").into());
+    }
+    
     return Err(APIError::err("post_report_not_implemented").into());
   }
 }
