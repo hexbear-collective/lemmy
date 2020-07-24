d8adb1e3d5dd06c4b8c6bd15165deb3c915e9b49
Porkroll Posadist <porkrollposadist@protonmail.com>
2020-07-24 15:26:53 -0400

    Check for community ban in EditCommunity

--- server/src/api/community.rs
+++ server/src/api/community.rs
@@ -5,28 +5,17 @@ use crate::{
   blocking,
   websocket::{
     server::{JoinCommunityRoom, SendCommunityRoomMessage},
-    UserOperation,
-    WebsocketInfo,
+    UserOperation, WebsocketInfo,
   },
   DbPool,
 };
 use lemmy_db::{
   community_settings::{CommunitySettings, CommunitySettingsForm},
-  naive_now,
-  Bannable,
-  Crud,
-  Followable,
-  Joinable,
-  SortType,
+  naive_now, Bannable, Crud, Followable, Joinable, SortType,
 };
 use lemmy_utils::{
-  generate_actor_keypair,
-  is_valid_community_name,
-  make_apub_endpoint,
-  naive_from_unix,
-  slur_check,
-  slurs_vec_to_str,
-  EndpointType,
+  generate_actor_keypair, is_valid_community_name, make_apub_endpoint, naive_from_unix, slur_check,
+  slurs_vec_to_str, EndpointType,
 };
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
@@ -392,6 +381,13 @@ impl Perform for Oper<EditCommunity> {
     let user = blocking(pool, move |conn| User_::read(conn, user_id)).await??;
     if user.banned {
       return Err(APIError::err("site_ban").into());
+    }
+
+    // Check for a community ban
+    let edit_id = data.edit_id;
+    let is_banned = move |conn: &'_ _| CommunityUserBanView::get(conn, user_id, edit_id).is_ok();
+    if blocking(pool, is_banned).await? {
+      return Err(APIError::err("community_ban").into());
     }
 
     // Verify it's a mod or admin
