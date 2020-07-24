a409378acb49dd40a3b82f9cdc2730c2b2412cd7
alltheseteeth <alltheseteeth@protonmail.com>
2020-07-24 14:36:04 -0700

    getting the prod deploy up to date with backup scripts

--- docker/chapo-dev/lemmydev-up.sh
+++ docker/chapo-dev/lemmydev-up.sh
@@ -7,4 +7,4 @@ export LEMMYDEV_JWT_SECRET="$(cat secrets/jwt_secret)"
 export LEMMYDEV_SMTP_PASSWORD="$(cat secrets/smtp_password)"
 export LEMMYDEV_HCAPTCHA_SECRET_KEY="$(cat secrets/hcaptcha_secret_key)"
 
-./docker-compose.sh up -d
+docker-compose up -d
--- docker/chapo-prod/lemmyprod-up.sh
+++ docker/chapo-prod/lemmyprod-up.sh
@@ -5,5 +5,6 @@ set -e
 export LEMMYPROD_POSTGRES_PW="$(cat secrets/postgres_pw)"
 export LEMMYPROD_JWT_SECRET="$(cat secrets/jwt_secret)"
 export LEMMYPROD_SMTP_PASSWORD="$(cat secrets/smtp_password)"
+export LEMMYPROD_HCAPTCHA_SECRET_KEY="$(cat secrets/hcaptcha_secret_key)"
 
-./docker-compose.sh up -d
+docker-compose up -d
