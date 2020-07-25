ef7ab2e3e16aa0b46e034793abac5a048f892761
Porkroll Posadist <porkrollposadist@protonmail.com>
2020-07-24 21:44:21 -0400

    Regenerate site_view in migration to include new column

--- server/migrations/2020-07-25-002456_add_create_community_toggle/down.sql
+++ server/migrations/2020-07-25-002456_add_create_community_toggle/down.sql
@@ -1,1 +1,13 @@
 alter table site drop column enable_create_communities;
+
+drop view site_view;
+create view site_view as 
+select *,
+(select name from user_ u where s.creator_id = u.id) as creator_name,
+(select avatar from user_ u where s.creator_id = u.id) as creator_avatar,
+(select count(*) from user_) as number_of_users,
+(select count(*) from post) as number_of_posts,
+(select count(*) from comment) as number_of_comments,
+(select count(*) from community) as number_of_communities
+from site s;
+
