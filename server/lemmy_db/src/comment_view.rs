626b0082ac94e95d4b9b2e6d2f2fc127b417d2ec
DashEightMate <dhas8m@protonmail.com>
2021-03-26 10:13:30 -0400

    show unlisted posts and comments on user page

--- server/lemmy_db/src/comment_view.rs
+++ server/lemmy_db/src/comment_view.rs
@@ -225,7 +225,7 @@ impl<'a> CommentQueryBuilder<'a> {
     };
 
     //in these cases listingtype doesn't matter
-    if self.for_post_id.is_none() && self.for_comment_ids.is_none() && self.for_community_id.is_none() {
+    if self.for_post_id.is_none() && self.for_comment_ids.is_none() && self.for_community_id.is_none() && self.for_creator_id.is_none() {
       query = match self.listing_type {
         ListingType::Subscribed => query.filter(subscribed.eq(true)),
         ListingType::Local => query.filter(community_local.eq(true)),
